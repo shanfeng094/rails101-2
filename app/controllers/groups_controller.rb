5debb8a79e3d83d66a1fcec37e55a08eef63d200
shanfeng094 <1842114650@qq.com>
2017-09-06 16:17:13 +0800

    move sqlite3 to dev group & add pg to production group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,4 +3,17 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+  def new
+    @group = Group.new
+  end
+  def destroy
+
+  end
+
+  def edit
+
+  end
+  def update
+
+  end
 end
